3d96be9d94eccc02a28c7be033faed4d4bfaddd3
Eric Drouillard <eric.drouillard@gmail.com>
2023-03-22 21:00:03 -0400

    minimum viable to-do

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+interface Todoitem {
+  Id: string;
+  Name: string;
+  Done?: boolean;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState("")
+  const [listItems, setListItems] = useState<Todoitem[]>([])
+
+  const addtodoitem = () => {
+    if (userInput === "") {
+      return;
+    }
+
+    setListItems( // a variable thats value is function.
+      (existingItems) => {
+          return [ // return a brand new array
+          ...existingItems, // with all of the previous todo items
+          { Id: uuidv4(), Name: userInput } // and a new item
+        ]
+      }
+    )
+    setUserInput("") // clear the user input
+  }
+
+  const markDone = (clickedItemId: string) => {
+    // 2. we want to update a single item that user has clicked to be done
+    // 3. done means `item.Done = true`
+    setListItems(existingItems => existingItems.map(existingItem => { // visit an item in the array
+      if (existingItem.Id === clickedItemId) { // we only care about the item that was clicked
+        existingItem.Done = true; // set the 'Done' property to the value 'true'
+      }
+      return existingItem; // return that item to the position in the array
+    }));
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     <h1>My To-Do List</h1>
+     <div>
+      <input type={"text"} placeholder={"What would you like to do?"} value={userInput} onChange={(event)=>setUserInput(event.currentTarget.value)}/>
+      <p><button onClick={addtodoitem}>Add Your Item</button></p>
+     </div>
+     <div>
+      {listItems.map((item: Todoitem) => 
+      <p onClick={()=> markDone(item.Id)} key={item.Id}>
+        {item.Done ? // inline if statement, a ternary
+          <span style={{textDecoration: 'line-through'}}>{item.Name}</span> : 
+          item.Name}
+      </p>)}
+     </div>
+    </div>  
   )
 }
 
